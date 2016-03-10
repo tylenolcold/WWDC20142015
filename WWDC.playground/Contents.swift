f3b249aa6dfdec78d0704b1f6cead263f96d349a
tylenolcold <jimlee1982@hotmail.com>
2016-03-11 00:51:17 +0800

    Added files via upload

---
+++ WWDC.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+/*
+1、苹果近两年WWDC都发布了什么新技术?
+2014：
+发布全新桌面操作系统优胜美地(Yosemite)、推出最新一代移动操作系统iOS 8及开发者应用。
+a. 全新的Mac OS X 10.10 Yosemite
+b. iOS 8更多功能上的改进
+c. 信息功能升级
+d. 指纹识别功能
+2015:
+发布了iOS 9、OS X和Watch OS 2三大系统的更新
+a. 智能手表终将摆脱手机
+b. 可穿戴银行即将到来
+c. 以智能穿戴为中心的智能家居正在形成
+*/
